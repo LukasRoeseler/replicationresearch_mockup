9a7e2ef30a7caf5126516d140703e6dc066d128c
LukasRoeseler <lukas.roeseler@uni-muenster.de>
2024-08-16 10:37:52 +0200

    Added static collection

---
+++ generate_collection.R
@@ -0,0 +1,17 @@
+
+# Create table for replication collection ---------------------------------
+
+
+
+### open dataset
+devtools::install_github("forrtproject/FReD")
+library(FReD)
+ds <- load_fred_data()
+
+### create data
+ds <- ds[, c("discipline", "description", "ref_original", "ref_replication")]
+refs <- ds[-which(duplicated(ds$ref_replication)), ]
+names(refs) <- c("Discipline", "Description of Finding", "Original Report", "Replication Report")
+
+### print table
+write.csv(refs, file = "replication_collection.csv")
